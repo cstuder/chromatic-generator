594868c90556c691a8b1de247eea3e8dbc7aea1e
christian studer <cstuder@existenz.ch>
2015-10-13 00:14:58 +0200

    Work on the script

--- chromatic-generator.py
+++ chromatic-generator.py
@@ -1,32 +1,58 @@
 # chromatic-generator.py
-import glob, os, argparse
+import glob, os, argparse, shutil
 from PIL import Image
 
 # Decode command line arguments
-parser = argparse.ArgumentParser(description='Prepares images and creates a JSON file for consumption with chromatic.js')
+parser = argparse.ArgumentParser(description='Creates beautiful HTML photo galleries with the help of chromatic.js')
 
 parser.add_argument('inputdir', help='Source directory with the images')
-parser.add_argument('outputdir', help='Target directory for resized images, thumbnails and JSON file')
-parser.add_argument('--urlprefix', help='Prefix added to the filename in the JSON file', default='')
-parser.add_argument('-f', '--force', help='Overwrite image files in target directory')
+parser.add_argument('outputdir', help='Target directory for the gallery files')
+parser.add_argument('-f', '--force', action='store_true', help='Overwrite files in target directory')
 
-parser.add_argument('--width', help='Maximum width of resized images')
-parser.add_argument('--height', help='Maximum height of resized images')
+parser.add_argument('-tw', '--thumbwidth', help='Maximum width of the thumbnails', default=800)
+parser.add_argument('-th', '--thumbheight', help='Maximum height of the thumbnails', default=800)
 
 args = parser.parse_args()
 
-if args.inputdir == args.outputdir:
-    raise ValueError('Source and target directory can not be identical')
+# Validate inputs
+inputdir = os.path.abspath(args.inputdir)
+outputdir = os.path.abspath(args.outputdir)
 
-# Copy images
+if inputdir == outputdir:
+    raise ValueError('Source and target directory cannot be identical')
 
-# TODO
+thumbnailsize = args.thumbwidth, args.thumbheight
 
-# Create thumbnails
+# Prepare target directories
+if not os.path.isdir(outputdir):
+    os.makedirs(outputdir)
 
-# TODO
+os.makedirs(outputdir + '/images')
+os.makedirs(outputdir + '/thumbnails')
+os.makedirs(outputdir + '/chromatic')
 
-# Create JSON
+# Prepare gallery files
+chromaticdir = os.path.dirname(os.path.abspath(__file__)) + '/chromatic.js'
+shutil.copy2(chromaticdir + '/dist/chromatic.js', outputdir + '/chromatic/chromatic.js')
+shutil.copy2(chromaticdir + '/lib/jquery-2.1.1.min.js', outputdir + '/chromatic/jquery.js')
+shutil.copy2(chromaticdir + '/stylesheets/chromatic.css', outputdir + '/chromatic/chromatic.css')
+
+# Find all images
+sourceImages = glob.glob(inputdir + '/*.jpg')
+
+for sourceImage in sourceImages:
+    filename = os.path.basename(sourceImage)
+
+    # Copy images
+    shutil.copy2(sourceImage, outputdir + '/images/' + filename)
+
+    # Create thumbnails
+
+    # TODO
+
+    # Store information and aspect ratio
+
+# Create index.html
 
 # TODO
 
